6cdcc647e003c1cda8535bf2a26a83a31a5c6224
Tinahiii <tinahi1992@gmail.com>
2025-07-13 21:05:04 +0300

    Add 10-factorial.js

---
+++ 10-factorial.js
@@ -0,0 +1,12 @@
+// 10-factorial.js
+
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const input = Number(process.argv[2]);
+
+console.log(factorial(isNaN(input) ? 0 : input));
