aa3721919183621abd078de070a8c2592da770ea
Tinahiii <tinahi1992@gmail.com>
2025-07-13 19:11:21 +0300

    Add 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,3 +1,4 @@
-console.log("C is Fun")
-console.log("Phyton is cool")
-console.log("Javascript is amazing")
+console.log("C is fun")
+console.log("Python is cool")
+console.log("JavaScript is amazing")
+console.log("Heloo")
